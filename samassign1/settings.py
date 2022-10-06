87a4e767fc6bf81cc0bbeaa29d571b77b51ac049
samsevens <samrosemount@gmail.com>
2022-10-05 22:05:51 -0400

    setup server

--- samassign1/settings.py
+++ samassign1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s$8%-)&&4*2*gd!b7wj*-7mum%rk&lx9pk6)#s!-b&=2oa7(*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
